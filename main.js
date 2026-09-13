fcc366f9cc7db8ca2654ce830b84f4edb316ecb7
Viktor Ekström <viktor.ekstrom@lerniastudent.se>
2026-09-13 13:34:23 +0200

    lagt till javaskript för att ändra texten med knappen

---
+++ main.js
@@ -0,0 +1,5 @@
+let header = document.getElementById("header");
+let button = document.querySelector("button");
+button.addEventListener("click", function () {
+    header.innerHTML = "Goodbye World";
+})
